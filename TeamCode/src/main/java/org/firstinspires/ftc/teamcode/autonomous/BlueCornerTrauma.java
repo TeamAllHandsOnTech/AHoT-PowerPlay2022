f0e889893073f2917e023001d33a6d896a69b3e6
Karen <51937640+KarenZhang0@users.noreply.github.com>
2023-01-16 10:15:47 -0800

    Color values and Trauma code updated to be like Hazard
    Color values weren't working so I updated the color values (green is constantly sketchy, make sure to test zone 1 a lot?) Since we're working with Trauma, I also copied over the code to Trauma (still haven't done it for red)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueCornerTrauma.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueCornerTrauma.java
@@ -1,47 +1,225 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.DriveDirections;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
 
-
-@Autonomous(name="BlueCorner", group="Trauma")
-
-public class BlueCornerTrauma extends DriveDirections {
+@Autonomous(name="BlueCornerTrauma", group="A")
+public class BlueCornerTrauma extends DriveDirections
+{
+    OpenCvWebcam webcam;
+    protected int zone;
+    int finalZone;
 
     private ElapsedTime runtime = new ElapsedTime();
-    private double moveSpeed = 0.3;
-
+    private double moveSpeed = 0.6;
 
     @Override
-    public void runOpMode() {
+    public void runOpMode(){
+
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"));
+
+        webcam.setPipeline(new SamplePipeline());
+
+        webcam.setMillisecondsPermissionTimeout(1000); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode){
+
+            }
+        });
 
         isHazard = false;
 
         super.runOpMode();
 
-        // Wait for the game to start (driver presses PLAY)
+        initArm();
+
         waitForStart();
+
+        finalZone = zone;
+
+        telemetry.addData("Final Zone: ", finalZone);
+        telemetry.addData("Zone: ", zone);
+        telemetry.addData("arm height: ", getArmHeight());
+        telemetry.update();
+
+        closeClaw();
+
         runtime.reset();
+        armToHeight(100);
+        armMotor.setPower(0.1);
 
-        //Pushes cone onto junction
-        DriveForDistance(moveSpeed, .52, "RIGHT");
-        DriveForDistance(moveSpeed, .08, "BACKWARD");
-        DriveForDistance(moveSpeed, .29, "FORWARD");
-        DriveForDistance(moveSpeed, .3, "BACKWARD");
-        DriveForDistance(moveSpeed, .36, "LEFT");
-        DriveForDistance(moveSpeed, .75, "FORWARD");
+        StraightDrive(moveSpeed, 0.89, "LEFT");
+        DriveForTime("BACKWARD",moveSpeed, 0.3);
+        StraightDrive(0.4, 0.68, "FORWARD");
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+        armToHeight(925);
+        armMotor.setPower(0.1);
 
-
+        StraightDriveNoSlow(0.2, 0.1, "FORWARD");
 
 
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
+        sleep(500);
+
+        openClaw();
+
+        sleep(500);
+
+        armMotor.setPower(0);
+
+        StraightDrive(moveSpeed, 0.05, "BACKWARD");
+        StraightDrive(moveSpeed, 0.4, "RIGHT");
+        StraightDrive(moveSpeed, 0.53, "FORWARD");
+
+        sleep(750);
+
+        rotateToZAbs(-90, 0);
+
+        sleep(500);
+
+
+        StraightDrive(moveSpeed, 1.12, "FORWARD");
+
+        armMotor.setPower(-0.1);
+
+        sleep(500);
+
+        armMotor.setPower(0.1);
+
+        closeClaw();
+
+        sleep(500);
+
+        armToHeight(500);
+        armMotor.setPower(0.1);
+
+        sleep(500);
+
+        StraightDrive(moveSpeed, 1.1, "BACKWARD");
+
+        armToHeight(100);
+
+        sleep(750);
+
+        rotateToZAbs(180, 0);
+
+        sleep(750);
+
+        StraightDrive(moveSpeed, .45, "FORWARD");
+
+        StraightDrive(moveSpeed, .23, "LEFT");
+
+        armToHeight(550);
+
+        armMotor.setPower(0.1);
+
+        sleep(500);
+
+        StraightDrive(0.2, .09, "FORWARD");
+
+        armMotor.setPower(-0.2);
+        sleep(300);
+
+        armMotor.setPower(0);
+        openClaw();
+
+        sleep(500);
+
+
+        telemetry.addData("Final Zone: ", finalZone);
+        telemetry.addData("Zone: ", zone);
+        telemetry.update();
+
+        switch(finalZone){
+            case 1:
+                StraightDrive(moveSpeed,.3, "RIGHT");
+                break;
+            case 2:
+                StraightDrive(moveSpeed, .4, "LEFT");
+                break;
+            case 3:
+                StraightDrive(moveSpeed, 1.1, "LEFT");
+                break;
+        }
+
+        while (opModeIsActive()){
+
+        }
+    }
+
+
+    class SamplePipeline extends OpenCvPipeline{
+        Mat HSVimage = new Mat();
+        Mat greenImage = new Mat();
+        Mat blueImage = new Mat();
+        Mat pinkImage = new Mat();
+
+        double greenPixels;
+        double bluePixels;
+        double pinkPixels;
+
+        boolean viewportPaused;
+
+        Scalar greenLower = new Scalar(30, 75, 75);
+        Scalar greenHigher = new Scalar(90, 255, 255);
+
+        Scalar blueLower = new Scalar(90, 110, 50);
+        Scalar blueHigher = new Scalar(100, 255, 255);
+
+        Scalar pinkLower = new Scalar(150, 100, 100);
+        Scalar pinkHigher = new Scalar(170, 200, 255);
+
+        /*
+         * NOTE: if you wish to use additional Mat objects in your processing pipeline, it is
+         * highly recommended to declare them here as instance variables and re-use them for
+         * each invocation of processFrame(), rather than declaring them as new local variables
+         * each time through processFrame(). This removes the danger of causing a memory leak
+         * by forgetting to call mat.release(), and it also reduces memory pressure by not
+         * constantly allocating and freeing large chunks of memory.
+         */
+
+        @Override
+        public Mat processFrame(Mat input) {
+            Imgproc.cvtColor(input, HSVimage, Imgproc.COLOR_RGB2HSV);
+
+            Core.inRange(HSVimage, greenLower, greenHigher, greenImage);
+            Core.inRange(HSVimage, blueLower, blueHigher, blueImage);
+            Core.inRange(HSVimage, pinkLower, pinkHigher, pinkImage);
+
+            greenPixels = Core.sumElems(greenImage).val[0];
+            bluePixels = Core.sumElems(blueImage).val[0];
+            pinkPixels = Core.sumElems(pinkImage).val[0];
+
+            if(greenPixels > bluePixels && greenPixels > pinkPixels){
+                zone = 1;
+            } else if(bluePixels > pinkPixels){
+                zone = 2;
+            } else {
+                zone = 3;
+            }
+
+            return null;
         }
     }
 }
-
