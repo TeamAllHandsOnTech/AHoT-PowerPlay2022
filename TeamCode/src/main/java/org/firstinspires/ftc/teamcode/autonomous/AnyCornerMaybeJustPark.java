ff96fac98cbf65bbea6d5e82a2a6a5550d1c28a2
BHill256 <bricelikesrobots@gmail.com>
2023-02-24 12:22:05 -0800

    Added possible park only auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AnyCornerMaybeJustPark.java
@@ -0,0 +1,126 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.DriveDirections;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous(name="AnyCornerMaybeJustPark", group="A")
+public class AnyCornerMaybeJustPark extends DriveDirections
+{
+    OpenCvWebcam webcam;
+    protected int zone;
+    int finalZone;
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private double moveSpeed = 0.6;
+
+    @Override
+    public void runOpMode(){
+
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"));
+
+        webcam.setPipeline(new SamplePipeline());
+
+        webcam.setMillisecondsPermissionTimeout(1000); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode){
+
+            }
+        });
+
+        super.runOpMode();
+
+        waitForStart();
+
+        finalZone = zone;
+
+        telemetry.addData("Final Zone: ", finalZone);
+        telemetry.addData("Zone: ", zone);
+        telemetry.update();
+
+        straightDrive(0.5, 0.1, "RIGHT");
+        straightDrive(0.5, 0.7, "FORWARD");
+
+        switch(finalZone){
+            case 1:
+                straightDrive(moveSpeed,.6, "LEFT");
+                break;
+            case 2:
+                break;
+            case 3:
+                straightDrive(moveSpeed, .6, "RIGHT");
+                break;
+        }
+
+        while (opModeIsActive()){
+
+        }
+    }
+
+
+    class SamplePipeline extends OpenCvPipeline{
+        Mat HSVimage = new Mat();
+        Mat greenImage = new Mat();
+        Mat blueImage = new Mat();
+        Mat pinkImage = new Mat();
+
+        double greenPixels;
+        double bluePixels;
+        double pinkPixels;
+
+        boolean viewportPaused;
+
+        Scalar greenLower = new Scalar(30, 75, 75);
+        Scalar greenHigher = new Scalar(90, 255, 255);
+
+        Scalar blueLower = new Scalar(90, 110, 50);
+        Scalar blueHigher = new Scalar(100, 255, 255);
+
+        Scalar pinkLower = new Scalar(150, 100, 100);
+        Scalar pinkHigher = new Scalar(170, 200, 255);
+
+
+
+        @Override
+        public Mat processFrame(Mat input) {
+            Imgproc.cvtColor(input, HSVimage, Imgproc.COLOR_RGB2HSV);
+
+            Core.inRange(HSVimage, greenLower, greenHigher, greenImage);
+            Core.inRange(HSVimage, blueLower, blueHigher, blueImage);
+            Core.inRange(HSVimage, pinkLower, pinkHigher, pinkImage);
+
+            greenPixels = Core.sumElems(greenImage).val[0];
+            bluePixels = Core.sumElems(blueImage).val[0];
+            pinkPixels = Core.sumElems(pinkImage).val[0];
+
+            if(greenPixels > bluePixels && greenPixels > pinkPixels){
+                zone = 1;
+            } else if(bluePixels > pinkPixels){
+                zone = 2;
+            } else {
+                zone = 3;
+            }
+
+            return null;
+        }
+    }
+}
